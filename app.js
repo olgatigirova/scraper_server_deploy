980d0d914b796041fc8f410e3db92f8a0b2dd1f8
Olga Tigirova <olga.tigirova@do.local>
2017-02-16 18:47:08 +0200

    docs host is modified to heroku

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ const options = {
 
 const spec = fs.readFileSync('./api/swagger.yaml', 'utf8');
 const swaggerDoc = jsyaml.safeLoad(spec);
+swaggerDoc.host = `scraper-server-deploy.herokuapp.com:${serverPort}`; 
 
 swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
   app.use(middleware.swaggerMetadata());
